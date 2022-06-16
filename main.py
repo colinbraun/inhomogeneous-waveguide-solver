cceb80a32bd539543b6998a09ea33839e30158d2
Colin Braun <electrolitic21@gmail.com>
2022-06-16 03:10:39 -0500

    Add 1x0.5 size meshes

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     # connectivity, all_nodes, all_edges, boundary_node_numbers, boundary_edge_numbers, remap_inner_node_nums, remap_inner_edge_nums, all_edges_map = load_mesh("rectangular_waveguide.inp", 1, [1])
 
     # Load a homogeneous rectangular waveguide with epsilon_r = 1
-    waveguide = Waveguide("rect_mesh_two_epsilons_coarse.inp", ["EB1", "EB2"], "EB3")
+    # waveguide = Waveguide("rect_mesh_two_epsilons_coarse.inp", ["EB1", "EB2"], "EB3")
+    waveguide = Waveguide("rect_mesh_one_epsilon_finer_20220615.inp", ["EB1"], "EB2")
     # Solve for the propagation constants and eigenvectors.
     betas, all_eigenvectors, k0s = waveguide.solve()
     # Plot the dispersion curve using the results.
@@ -29,7 +30,7 @@ if __name__ == '__main__':
     # Indexing that set by [-1] gives the mode with the highest propagation constant for that k0.
     # This is usually but not always the first propagating mode. It is always the highest beta / k0 value at the
     # particular k0 value on the dispersion graph (see Waveguide#plot_dispersion()).
-    waveguide.set_mode_index(2)
+    waveguide.set_mode_index(0)
     # waveguide.plot_fields(all_eigenvectors[-1][-1])
     waveguide.plot_fields()
     print(f"Plot shown for beta/k0 = {betas[-1]/k0s[-1]}")
